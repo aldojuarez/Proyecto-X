44c374788f0b6f78736a8a799cafd578ac4d76e7
Dayana Esquivel <dayanaesquivel4@outlook.com>
2020-02-24 13:30:49 -0700

    Funciones de la suma

---
+++ calculos.js
@@ -0,0 +1,7 @@
+function suma(){
+    var a=document.getElementById('').value;
+    var b=document.getElementById('').value;
+    var sum= a+b;
+    document.getElementById("res").innerHTML= sum;
+    console.log("perro")
+}
