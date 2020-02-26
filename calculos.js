d813226c700b86f0007e489e259d91e992c9016f
Dayana Esquivel <dayanaesquivel4@outlook.com>
2020-02-26 09:57:35 -0700

    Agrege metodo pantalla, eliminar todo y resta

--- calculos.js
+++ calculos.js
@@ -1,7 +1,24 @@
-function suma(){
-    var a=document.getElementById('n1').value;
-    var b=document.getElementById('n2').value;
-    var sum= a+b;
-    document.getElementById("res").innerHTML= sum;
-    console.log("Hola")
+
+function agregarNumero(numero){
+    var mostrar="";
+    var pantalla= document.getElementById("textoPantalla").value;
+    console.log(pantalla);
+    mostrar= pantalla + numero;
+    document.getElementById("textoPantalla").value = mostrar;
+    
+}
+
+function resta(){
+    var pant=document.getElementById("textoPantalla").value;
+    var resta = pant.indexOf("-");
+    if(resta!=-1){
+        var primero= pant.split("-");
+        var resultado= parseInt(primero[0])-parseInt(primero[1]);
+        borradoTotal();
+        document.getElementById("textoPantalla").value=resultado;
+    }
+}
+
+function borradoTotal(){
+    document.getElementById("textoPantalla").value = "";
 }
