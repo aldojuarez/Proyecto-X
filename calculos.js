e2df856dad4f08065552d0bd859c25923cc4124a
unknown <m.angelgt88@gmail.com>
2020-03-01 23:25:41 -0700

    cambio 2

--- calculos.js
+++ calculos.js
@@ -53,4 +53,32 @@ function division(){
 function borradoTotal(){
     document.getElementById("textoPantalla").value = "";
 }
+function resultado(){
+    var pant=document.getElementById("textoPantalla").value;
+    var suma = pant.indexOf("+");
+    var resta = pant.indexOf("-");
+    var multi = pant.indexOf("*");
+    var division = pant.indexOf("/");
+    if(suma!=-1){
+        var primero= pant.split("+");
+        var resultado= parseInt(primero[0])+parseInt(primero[1]);
+        borradoTotal();
+        document.getElementById("textoPantalla").value=resultado;
+    }else if(resta!=-1){
+        var primero= pant.split("-");
+        var resultado= parseInt(primero[0])-parseInt(primero[1]);
+        borradoTotal();
+        document.getElementById("textoPantalla").value=resultado;
+    }else if(multi!=-1){
+        var primero= pant.split("*");
+        var resultado= parseInt(primero[0])*parseInt(primero[1]);
+        borradoTotal();
+        document.getElementById("textoPantalla").value=resultado;
+    } else if(division!=-1){
+        var primero= pant.split("/");
+        var resultado= parseInt(primero[0])/parseInt(primero[1]);
+        borradoTotal();
+        document.getElementById("textoPantalla").value=parseFloat(resultado);
+    }
+} 
 
